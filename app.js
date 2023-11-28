e57257797e23bb637e22cbe61c8fa4a9b73a08da
abdullah <lanrequadir6o@gmail.com>
2023-11-28 14:20:23 +0100

    fourth commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost:27017/shopping');
+mongoose.connect('mongodb://127.0.0.1:27017/shopping');
 
 
 // view engine setup
