7b5b37aea8cee8b056801bbce0f23d274bc580be
abdullah <lanrequadir6o@gmail.com>
2023-12-06 08:34:03 +0100

    fifth commit

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,19 @@ var router = express.Router();
 var Product = require("../models/product");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  var products = Product.find();
-  res.render("shop/index", { title: "shopping cart", products: products });
+router.get("/", async function (req, res, next) {
+  try {
+    const docs = await Product.find();
+    const productChuncks = [];
+    const chunckSize = 3;
+    for (var i = 0; i < docs.length; i+= chunckSize){
+      productChuncks.push(docs.slice(i, i + chunckSize))
+    }
+    res.render("shop/index", { title: "shopping cart", products: productChuncks });
+  } catch (err) {
+    // Handle the error appropriately, e.g., send an error response or log it
+    res.status(500).send("An error occurred: " + err.message);
+  }
 });
 
 module.exports = router;
--- seed/product-seeders.js
+++ seed/product-seeders.js
@@ -1,37 +1,62 @@
 var Product = require("../models/product");
-
 var mongoose = require("mongoose");
-//mongodb+srv://admin:<password>@cluster0.x0bskjb.mongodb.net/
 
-//mongoose.connect("localhost:27017/shopping");
-mongoose.connect('mongodb://127.0.0.1:27017/shopping');
+// Connect to the MongoDB database
+mongoose.connect("mongodb://127.0.0.1:27017/shopping");
 
-var products = [
-  new Product({
-    ImagePath:
-      "https://th.bing.com/th/id/OIP.hihxM9RoILsIVgV1FNMZ4wAAAA?rs=1&pid=ImgDetMain",
-    title: "mobile phone",
-    description: "apple iphone",
-    price: 10,
-  }),
-  new Product({
-    ImagePath:
-      "https://th.bing.com/th/id/OIP.hihxM9RoILsIVgV1FNMZ4wAAAA?rs=1&pid=ImgDetMain",
-    title: "mobile phone",
-    description: "apple iphone",
-    price: 10,
-  }),
-];
-
-for (var i = 0; i < products.length; i++) {
-  products[i].save(function (err, result) {
-    done++;
-    if (done === products.length) {
-      exit();
+// Function to save products to the database
+async function saveProducts(products) {
+  try {
+    for (let i = 0; i < products.length; i++) {
+      await products[i].save();
     }
-  });
+    console.log("Products saved successfully!");
+  } catch (err) {
+    console.error("Error saving products:", err);
+  } finally {
+    exit(); // Disconnect from the database after saving (or encountering an error)
+  }
 }
 
+// Function to disconnect from the database
 function exit() {
   mongoose.disconnect();
 }
+
+// Array of product instances
+var products = [
+  new Product({
+    imagepath: "https://tpmobile.vn/wp-content/uploads/2019/11/637037652462548298_11-trang-768x768.jpg",
+    title: "apple iphone",
+    description: "iphone 11",
+    price: 20,
+  }),
+  new Product({
+    imagepath: "https://sotohit.ru/d/iphone_12_black.png",
+    title: "apple iphone",
+    description: "iphone 12",
+    price: 30,
+  }),
+  new Product({
+    imagepath: "https://th.bing.com/th/id/OIP.GADafLPEaobF4-IjDGJhyAHaHa?rs=1&pid=ImgDetMain",
+    title: "apple iphone",
+    description: "iphone 13",
+    price: 40,
+  }),
+  new Product({
+    imagepath: "https://4gpackages.com/wp-content/uploads/2022/03/Apple-iPhone-13-Mini.jpg",
+    title: "apple iphone",
+    description: "iphone 14 ",
+    price: 50,
+  }),
+  new Product({
+    imagepath: "https://i0.wp.com/9to5mac.com/wp-content/uploads/sites/6/2022/09/iPhone-15-Pro.jpg?w=1024&quality=82&strip=all&ssl=1",
+    title: "apple iphone",
+    description: "iphone 15",
+    price: 60,
+  }),
+  // Add other products similarly...
+];
+
+// Save the products to the database
+saveProducts(products);
